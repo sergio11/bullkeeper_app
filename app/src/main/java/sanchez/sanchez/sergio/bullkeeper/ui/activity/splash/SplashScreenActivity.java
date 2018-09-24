d595b2bdec1650e1856c15eba82411ba9679f6cc
Sergio Sánchez Sánchez <ssanchez@ebikemotion.com>
2018-09-24 22:24:34 +0200

    MAA_72: The application should show the main screen if there is an active session token. (51).

--- app/src/main/java/sanchez/sanchez/sergio/bullkeeper/ui/activity/splash/SplashScreenActivity.java
+++ app/src/main/java/sanchez/sanchez/sergio/bullkeeper/ui/activity/splash/SplashScreenActivity.java
@@ -2,33 +2,50 @@ package sanchez.sanchez.sergio.bullkeeper.ui.activity.splash;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
-
 import javax.inject.Inject;
 
 import sanchez.sanchez.sergio.bullkeeper.AndroidApplication;
 import sanchez.sanchez.sergio.bullkeeper.R;
 import sanchez.sanchez.sergio.bullkeeper.di.components.ApplicationComponent;
 import sanchez.sanchez.sergio.bullkeeper.navigation.INavigator;
-import timber.log.Timber;
+import sanchez.sanchez.sergio.domain.repository.IPreferenceRepository;
+import sanchez.sanchez.sergio.domain.utils.IAppUtils;
 
 /**
  * Splash Screen Activity
  */
 public class SplashScreenActivity extends AppCompatActivity {
 
+    /**
+     * Navigator
+     */
     @Inject
     protected INavigator navigator;
+
+    /**
+     * Preference Manager
+     */
+    @Inject
+    protected IPreferenceRepository preferenceRepository;
+
+    /**
+     * App Utils
+     */
+    @Inject
+    protected IAppUtils appUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initializeInjector();
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        navigator.navigateToIntro();
+
+        if(appUtils.isValidString(preferenceRepository.getAuthToken())) {
+            navigator.navigateToHome();
+        } else {
+            navigator.navigateToIntro();
+        }
+
         finish();
     }
 
